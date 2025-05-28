9d83ca63255f64b24ef2f8f338bd7d94e7a54d51
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-05-28 16:31:14 +0300

    Add Async Barrier

---
+++ Task01.Barrier.Cancellation/Program.cs
@@ -0,0 +1,31 @@
+﻿StopperToken token = new();
+
+Console.WriteLine("Main: letting worker run for 5 sec");
+var waiter = Worker(); ;
+await Task.Delay(5000);
+token.IsStopped = true;
+Console.WriteLine("Main: waiting for worker to stop");
+await waiter;
+
+async Task Worker()
+{
+    await Task.Yield();
+    int x = 0;
+
+    while (!token.IsStopped)
+    {
+        x = UpdateItem(x);
+    }
+
+    Console.WriteLine($"Worker: stopped");
+}
+
+int UpdateItem(int item)
+{
+    return item;
+}
+
+public struct StopperToken
+{
+    public volatile bool IsStopped;
+}
