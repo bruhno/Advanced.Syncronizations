d8117b394c5a6301776c39e0e4649c52d82b8e29
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-05-28 12:05:56 +0300

    Add project files.

---
+++ Task01.Barrier/Program.cs
@@ -0,0 +1,37 @@
+﻿bool s_stopWorker = false;
+
+Console.WriteLine("Main: letting worker run for 5 sec");
+Thread t = new Thread(Worker);
+t.Start();
+Thread.Sleep(5000);
+s_stopWorker = true;
+Console.WriteLine("Main: waiting for worker to stop");
+t.Join();
+
+
+void Worker(object o)
+{
+    int x = 0;
+
+    // Problem: It can be locked in the Release mode
+    while (!s_stopWorker)
+    {
+        x++;
+    }
+
+    // Solution 1: Use Volatile.Read to ensure visibility of the stop signal
+    //while (!Volatile.Read(ref s_stopWorker))
+    //{
+    //    x++;
+    //}
+
+    // Solution 2: Use Thread.MemoryBarrier to ensure visibility of the stop signal
+    //while (!s_stopWorker)
+    //{
+    //    Thread.MemoryBarrier();
+    //    x++;
+    //}
+
+    Console.WriteLine($"Worker: stopped when x ={x}");
+}
+
