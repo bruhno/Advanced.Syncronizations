16da2c5a3f8fff0ef44c6762b1bcd883d09b25f2
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-06-04 09:49:24 +0300

    Interlocked Add

---
+++ Task09.InterlockedAdd/Program.cs
@@ -0,0 +1,15 @@
+﻿var sum = 0;
+
+
+Parallel.For(0, 100, _ => Increment());
+
+Console.WriteLine(sum);
+
+void Increment()
+{
+    for (var i = 0; i < 100; i++)
+    {
+        Interlocked.Add(ref sum, 1);
+        //sum = sum + 1;
+    }
+}
