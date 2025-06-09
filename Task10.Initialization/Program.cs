521bf40e07b5ff5e53c249991380261467d678f3
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-06-09 14:36:27 +0300

    MySpinLock

---
+++ Task10.Initialization/Program.cs
@@ -0,0 +1,30 @@
+﻿var value = -1;
+
+Parallel.For(1, 101, Init);
+
+Console.WriteLine($"Final value: {value}");
+
+void Init(int x)
+{
+
+    var newValue = CalcValue(x);
+
+    var original = Interlocked.Exchange(ref value, newValue);
+
+    if (original == -1)
+    {
+        Console.WriteLine($"Initialized: {newValue}");
+        return;
+    };
+
+    while (original != Interlocked.Exchange(ref value, original)) { }
+
+    //Console.WriteLine($"Initialized: {newValue}, original {original}");
+
+}
+
+int CalcValue(int x)
+{
+    Thread.Sleep(10);
+    return x;
+}
---
+++ Task11.SpinLock/Program.cs
@@ -0,0 +1,32 @@
+﻿var sum = 0;
+
+var myLock = new MySpinLock();
+
+Parallel.For(0, 10, _ => Increment());
+
+Console.WriteLine(sum);
+
+void Increment()
+{
+    for (var i = 0; i < 100000; i++)
+    {
+        myLock.Enter();
+        sum++;
+        myLock.Exit();
+    }
+}
+
+class MySpinLock
+{
+    public void Enter()
+    {
+        while (Interlocked.Exchange(ref _locked, true)) ;
+    }
+
+    public void Exit()
+    {
+        Interlocked.Exchange(ref _locked, false);
+    }
+
+    private bool _locked;
+}
