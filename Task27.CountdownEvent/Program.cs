c62341e81f4465e541122f7b220f3974989bb355
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-07-13 18:08:20 +0300

    *

---
+++ Task27.CountdownEvent/Program.cs
@@ -0,0 +1,19 @@
+﻿// Нужно дождаться завершения
+// определенного количества операций
+
+var N = 4;
+
+var cde = new CountdownEvent(N);
+
+Parallel.For(0, N, Work);
+
+cde.Wait();
+
+Console.WriteLine("All complete");
+
+void Work(int num)
+{
+    Thread.Sleep(500 * num);
+    var r = cde.Signal();
+    Console.WriteLine($"{num} done,  i'm last {r}");
+}
