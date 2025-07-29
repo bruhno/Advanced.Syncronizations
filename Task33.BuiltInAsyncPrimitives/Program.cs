065fd7b3fa53165eec4e8bef29c2e46c44e5ac81
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-07-29 22:49:46 +0300

    AsyncBarrier

---
+++ Task33.BuiltInAsyncPrimitives/Program.cs
@@ -0,0 +1,12 @@
+﻿var s = new SemaphoreSlim(3);
+
+Enumerable.Range(1, 9).Select(i => Task.Run(async () =>
+{
+    await s.WaitAsync();
+    await Task.Delay(2000);
+    Console.WriteLine($"{i} done");
+    s.Release();
+})).ToList();
+
+
+await Task.Delay(10000);
---
+++ Task35.AsyncBarrier.cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+var barrier = new AsyncBarrier(5);
+
+var random = new Random();
+
+Enumerable.Range(1, 5).Select(i => Task.Run(async () =>
+{
+    foreach (var phase in "ABCDEF")
+    {
+        Thread.Sleep(random.Next(300, 1300));
+        Console.WriteLine($"{i} completed {phase}");
+        await barrier.SignalAndWaitAsync();
+
+        Console.WriteLine($"--------");
+    }
+
+})).ToList();
+
+
+await Task.Delay(100000);
+
+class AsyncBarrier(int _participantCount)
+{
+    public Task SignalAndWaitAsync()
+    {
+        var tcs = _tcs;
+        
+        if (Interlocked.Increment(ref _currentCount) == _participantCount)
+        {
+            Interlocked.Exchange(ref _currentCount, 0);
+            _tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+            tcs.SetResult();
+        }
+
+        return tcs.Task;
+    }
+
+    TaskCompletionSource _tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+    int _currentCount = 0;
+}
