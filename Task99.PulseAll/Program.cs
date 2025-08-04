3faad31d01e6ce1c103a5927ccd071e4d84e27a8
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-08-04 08:07:38 +0300

    PulseAll for cancellation

--- Task99.PulseAll/Program.cs
+++ Task99.PulseAll/Program.cs
@@ -1,26 +1,63 @@
 ﻿// при каком сценарии потребовалось бы вызвать PulseAll?
 
+
+var N = 0;
+
 var q = new BoundedQueue<int>(5);
 
-_ = Task.Run(() =>
+Enumerable.Range(1, 3).Select(i => Task.Run(() =>
 {
     while (true)
     {
         Thread.Sleep(100);
-        var item = q.Dequeue();
-        Console.WriteLine($"Item : {item}");
+        try
+        {
+            var item = q.Dequeue();
+            Console.WriteLine($"{i} << : {item}");
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine($"{i} dequeue canceled");
+            return;
+        }
     }
-});
+})).ToList();
 
-Console.ReadKey();
-q.Enqueue(42);
-Console.ReadKey();
+
+Enumerable.Range(1, 2).Select(i => Task.Run(() =>
+{
+    while (true)
+    {
+        Thread.Sleep(300);
+
+        var item = Interlocked.Increment(ref N);
+
+        try
+        {
+            q.Enqueue(item);
+            Console.WriteLine($"{i} >> : {item}");
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine($"{i} enqueue canceled");
+            return;
+        }        
+    }   
+
+})).ToList();
+
+
+Thread.Sleep(1000);
+q.Cancel();
+Thread.Sleep(1000);
+
 
 class BoundedQueue<T>
 {
     private readonly Queue<T> _queue = new();
     private readonly int _maxSize;
     private readonly object _lock = new();
+    private bool _cancelled = false;
 
     public BoundedQueue(int maxSize)
     {
@@ -33,7 +70,16 @@ class BoundedQueue<T>
         {
             while (_queue.Count >= _maxSize)
             {
+                if (_cancelled)
+                {
+                    throw new OperationCanceledException();
+                }
                 Monitor.Wait(_lock);
+            }
+
+            if (_cancelled)
+            {
+                throw new OperationCanceledException();
             }
 
             _queue.Enqueue(item);
@@ -47,6 +93,12 @@ class BoundedQueue<T>
         {
             while (_queue.Count == 0)
             {
+                {
+                    if (_cancelled)
+                    {
+                        throw new OperationCanceledException();
+                    }
+                }
                 Monitor.Wait(_lock);
             }
 
@@ -54,6 +106,15 @@ class BoundedQueue<T>
             Monitor.Pulse(_lock);
 
             return item;
+        }
+    }
+
+    public void Cancel()
+    {
+        lock (_lock)
+        {
+            _cancelled = true;
+            Monitor.PulseAll(_lock);
         }
     }
 }
